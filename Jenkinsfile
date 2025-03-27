fa197050f8c996e42155f8e475539088c717d276
daddae022 <ing.addae@gmail.com>
2025-03-27 01:29:17 -0400

    Initial commit with Jenkinsfile and test setup

---
+++ Jenkinsfile
@@ -0,0 +1,14 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Setup') {
+            steps {
+                browserstack(credentialsId: '3be04f66-1c85-4b95-bdf5-216a0d4d796c') {
+                    echo 'Running Selenium tests on BrowserStack...'
+                    sh 'mvn test'
+                }
+            }
+        }
+    }
+}
---
+++ src/test/java/com/browserstack/test/SampleTest.java
@@ -0,0 +1,41 @@
+package com.browserstack.test;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.MutableCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+import java.util.HashMap;
+
+public class SampleTest {
+
+    @Test
+    public void testGoogleOnBrowserStack() throws Exception {
+        String username = System.getenv("BROWSERSTACK_USERNAME");
+        String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
+        String buildName = System.getenv("JENKINS_LABEL");
+
+        MutableCapabilities capabilities = new MutableCapabilities();
+        capabilities.setCapability("browserName", "Chrome");
+        capabilities.setCapability("browserVersion", "100.0");
+
+        HashMap<String, Object> browserstackOptions = new HashMap<>();
+        browserstackOptions.put("os", "Windows");
+        browserstackOptions.put("osVersion", "10");
+        browserstackOptions.put("sessionName", "Google Test via Jenkins");
+        browserstackOptions.put("buildName", buildName);
+        browserstackOptions.put("seleniumVersion", "4.0.0");
+
+        capabilities.setCapability("bstack:options", browserstackOptions);
+
+        WebDriver driver = new RemoteWebDriver(
+            new URL("https://" + username + ":" + accessKey + "@hub.browserstack.com/wd/hub"),
+            capabilities
+        );
+
+        driver.get("https://www.google.com");
+        System.out.println("Page title is: " + driver.getTitle());
+        driver.quit();
+    }
+}
